5d4309887907725b442fcea407025e2d1781b3cc
gouravpatidar326 <gouravpatidar326@gmail.com>
2026-07-13 19:18:17 +0530

    updated and cors set

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ const allowedOrigins = [
   'http://localhost:5175',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-  'http://127.0.0.1:5175'
+  'http://127.0.0.1:5175',
+  'https://human-hcm.netlify.app'
 ];
 
 app.use(cors({
